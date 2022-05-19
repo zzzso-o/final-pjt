5312910d48e5435ae1953c7e49d83e3df5a70edb
jisoo <1211ditto@gmail.com>
2022-05-19 10:00:00 +0900

    movie_urls

---
+++ final-pjt-back/movies/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'movies'
+
+urlpatterns = [
+    # home화면
+    path('', views.home, name='home'),
+    
+    # 인기 영화 조회
+    path('popularmovies/', views.popular_movies, name='popular_movies'),
+    # 인기 영화 상세페이지
+    path('popularmovies/<int:movies_pk>/', views.movie_detail, name='movie_detail'),
+    # 영화 평점 조회
+    path('<int:movie_pk>/comments/', views.movie_comments, name='movie_comments'),
+
+]
