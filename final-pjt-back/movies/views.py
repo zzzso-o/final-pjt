85df8ff088c02d7847475f63f39db72be7d7cb20
jisoo <1211ditto@gmail.com>
2022-05-19 10:43:12 +0900

    movies_views

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Movie, Comment
+
+from django.contrib.auth import get_user_model
+
+
+def home(requests):
+    pass
+
+def popular_movies(requests):
+    pass
+
+def movie_detail(requests):
+    pass
+
+def movie_comments(requests):
+    pass
