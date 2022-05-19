d1cfa31e80855f0f27a196667fcd801816afa144
jisoo <1211ditto@gmail.com>
2022-05-19 10:31:54 +0900

    accounts_views

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+
+
+def signup(requests):
+    pass
+
+def login(requests):
+    pass
+
+def logout(reuqests):
+    pass
+
+def porfile(reuqests):
+    pass
+
+def profile_articles(requests):
+    pass
+
+def profile_articles_comments(requests):
+    pass
+
+def profile_like_movies(requests):
+    pass
