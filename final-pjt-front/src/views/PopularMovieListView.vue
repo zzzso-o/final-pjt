ac4b5ad34dd79c3e57751773a2858fef6dd43d0f
2riing <2riing2@gmail.com>
2022-05-24 13:40:26 +0900

    0524

--- final-pjt-front/src/views/PopularMovieListView.vue
+++ final-pjt-front/src/views/PopularMovieListView.vue
@@ -1,50 +1,34 @@
 <template>
-  <div class=container>
-		<h1 class='title'>POPULAR MOVIES</h1>
-		
+  <div class="container">
+		<h1 class="title">POPULAR MOVIES</h1>
 
-		<div class="columns">
-		<div class="rows">
-		<div 
-    v-for="movie in popularMovies" :key="movie.id">
-
-		<div class="card column">
-			
-			<div class="card-image">
-				
-				<figure class="image">
-				<router-link :to="`/pouplar/${movie.id}`">
-				<img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="movie poster" class=" is-128x128">
-				</router-link>
-				</figure>
-			</div>
-			
-			<div class="card-content">
-				<div class="media">
-				<div class="media-content">
-					<p class="title is-5">{{movie.title}}</p>
+		<div class="row">
+			<div v-for="movie in popularMovies" :key="movie.id" class="card col-4">
+				<div class="card-image">
+					<figure class="image">
+						<router-link :to="`/pouplar/${movie.id}`">
+							<img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="movie poster" class=" is-128x128">
+						</router-link>
+					</figure>
 				</div>
+				<div class="card-content">
+					<div class="media">
+						<div class="media-content">
+							<p class="title is-5">{{movie.title}}</p>
+						</div>
+					</div>
+					<br>
+					<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
 				</div>
-				<br>
-				<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+				<footer class="card-footer">
+					<p class="card-footer-item">
+						<span>
+							<router-link :to="`popular/${movie.id}`"></router-link>
+						</span>
+					</p>
+				</footer>
 			</div>
-			<footer class="card-footer">
-				<p class="card-footer-item">
-					<span>
-						<router-link :to="`popular/${movie.id}`"></router-link>
-
-					</span>
-				</p>
-
-			</footer>
 		</div>
-		</div>
-		</div>
-
-
-
-		</div>
-
   </div>
 	
 </template>
@@ -55,18 +39,18 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
 	data() {
 		return {
-				items : [],
-				current:1,
-				perPage:12,
-				rangeBefore: 5,
-				rangeAfter: 5,
-				order: 'is-centered',
-				size: '',
-				isSimple: false,
-				isRounded: false,
-				hasInput: false,
-				inputPosition: '',
-				inputDebounce: '',
+			items : [],
+			current:1,
+			perPage:12,
+			rangeBefore: 5,
+			rangeAfter: 5,
+			order: 'is-centered',
+			size: '',
+			isSimple: false,
+			isRounded: false,
+			hasInput: false,
+			inputPosition: '',
+			inputDebounce: '',
 
 		}
 	},
