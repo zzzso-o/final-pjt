c07c80bd18354fd3b353650fccdc774e112d210f
jisoo <1211ditto@gmail.com>
2022-05-25 14:42:35 +0900

    0525_중간점검

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -2,5 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    # user_grade = models.IntegerField()
     pass
--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
 from .models import Movie, MovieComment
 
-# 전체 영화 조회(GET)
+User = get_user_model()
+
+# 전체 영화 조회
 class MovieListSerializer(serializers.ModelSerializer):
+
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ('pk', 'username')
+    
+    user = UserSerializer(read_only=True)
+    comment_count = serializers.IntegerField()
+    like_count = serializers.IntegerField()
+
+
     class Meta:
         model = Movie
-        fields = '__all__'
-        read_only_fields = ('like_user', 'movie_likes')
+        fields = ('pk', 'title', 'like_count', 'comments_count')
 
-# 상세 영화 조회 (GET)
+
+# 상세 영화 조회
 class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -17,15 +32,16 @@ class MovieSerializer(serializers.ModelSerializer):
         read_only_fields = ('like_user', 'movie_likes')
 
 
-# 상세 영화 전체 평점 조회(GET)
+# 상세 영화 전체 평점 조회
 class CommentSerializer(serializers.ModelSerializer):
-    comment_title = serializers.SerializerMethodField()
-    def get_comment_title(self, obj):
-        return obj.comment.title
-    userName = serializers.SerializerMethodField()
-    def get_userName(self,obj):
-        return obj.user.username
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ('pk', 'username')
+    
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = MovieComment
-        fields = ('id', 'userName', 'user', 'comment', 'comment_content', 'created_at',)
-        read_only_fields = ('user','comment',)
+        fields = ('pk', 'user', 'movie_comment_content', 'movie', 'movie_comment_created_at', 'movie_comment_updated_at')
+        read_only_fields = ('movie', 'movie_comment_created_at', 'movie_comment_updated_at')
